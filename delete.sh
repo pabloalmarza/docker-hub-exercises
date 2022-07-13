7ef22566f006344f8fb9a69677348c4f62629e03
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-13 20:39:58 +0100

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,4 +1,10 @@
-
 stack=hub-exercise
+echo "Deleting stack $stack"
 docker stack rm $stack
-docker volume rm $(docker volume ls -q | grep $stack)
+while [[ $(docker ps | grep hub-exercise | wc -l) != "0" ]] ;
+do
+ echo $(docker ps | grep hub-exercise | wc -l) "Containers still active"
+ sleep 4;
+done
+
+sleep 6
