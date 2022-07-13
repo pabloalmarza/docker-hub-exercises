0afe3c41c8328a8089c66189e21e67c8192f28bb
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-13 21:21:33 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@ exercise="exercise$1";
 stack=hub-exercise
 if [ -d $exercise ]
 then
+  if [ $1 eq "5" ]
+  then
+    docker secret create hub-exercise_WEBSERVER_CUSTOM_CERT_FILE $exercise/cert.cer
+    docker secret create hub-exercise_WEBSERVER_CUSTOM_KEY_FILE $exercise/key.pem
+  fi
   docker stack deploy -c $exercise/docker-compose.yml -c $exercise/docker-compose.local-overrides.yml -c $exercise/10sph.yaml hub-exercise
 else
   echo "Error: Directory $exercise does not exists."
