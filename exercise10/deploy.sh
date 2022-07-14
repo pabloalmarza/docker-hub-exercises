7f751aa2b694dc3526d0a968ddc7a2482916f9e4
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 13:12:35 +0100

    Update deploy.sh

--- exercise10/deploy.sh
+++ exercise10/deploy.sh
@@ -2,5 +2,16 @@ docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-override
 while ! $(docker inspect -f {{.State.Running}} $(docker ps -q -f name=cfssl)) ; do sleep 0.1; done;
 docker cp $1/healthcheck.sh $(docker ps -q -f name=cfssl):/usr/local/bin/docker-healthcheck.sh
 docker commit $(docker ps -q -f name=cfssl) blackducksoftware/blackduck-cfssl:1.0.7
-/bin/bash delete.sh
+stack=hub-exercise
+echo "Deleting stack $stack"
+docker stack rm $stack
+while [[ $(docker ps | grep hub-exercise | wc -l) != "0" ]] ;
+do
+ echo $(docker ps | grep hub-exercise | wc -l) "Containers still active"
+ sleep 4;
+done
+
+sleep 6
+docker volume rm $(docker volume ls -q | grep $stack)
+
 docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
