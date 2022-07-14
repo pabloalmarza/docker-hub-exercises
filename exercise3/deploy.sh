a40353ddbf0846261188a0ec1775c171918b7483
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 12:27:24 +0100

    Update deploy.sh

--- exercise3/deploy.sh
+++ exercise3/deploy.sh
@@ -1,3 +1,3 @@
-docker secret create hub-exercise_WEBSERVER_CUSTOM_CERT_FILE cert.cer
-docker secret create hub-exercise_WEBSERVER_CUSTOM_KEY_FILE key.pem
-docker stack deploy -c docker-compose.yml -c docker-compose.local-overrides.yml -c 10sph.yaml hub-exercise
+docker secret create hub-exercise_WEBSERVER_CUSTOM_CERT_FILE $1/cert.cer
+docker secret create hub-exercise_WEBSERVER_CUSTOM_KEY_FILE $1/key.pem
+docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
