192e60ce7aa759fa774b329ebcb3ea7adc86af7d
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 12:38:26 +0100

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -2,7 +2,6 @@ stack=hub-exercise
 echo "Deleting stack $stack"
 docker stack rm $stack
 docker secret rm $(docker secret ls --filter name=$stack -q)
-docker image rm $(docker image ls -f reference='blackducksoftware/blackduck-cfssl' -q)
 while [[ $(docker ps | grep hub-exercise | wc -l) != "0" ]] ;
 do
  echo $(docker ps | grep hub-exercise | wc -l) "Containers still active"
@@ -11,3 +10,4 @@ done
 
 sleep 6
 docker volume rm $(docker volume ls -q | grep $stack)
+docker image rm $(docker image ls -f reference='blackducksoftware/blackduck-cfssl' -q)
