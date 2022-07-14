2b9da3106036fb853869c789518ba556145a934c
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 12:36:48 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ exercise="exercise$1";
 if [ -d $exercise ]
 then
 	chmod 777 $exercise/deploy.sh
-	/bin/bash $exercise/deploy.sh
+	/bin/bash $exercise/deploy.sh $exercise
 else
   echo "Error: Directory $exercise does not exists."
 fi
