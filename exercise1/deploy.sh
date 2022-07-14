75a32797f310bc8e45c2be8fded466e6df7a95c9
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 12:24:24 +0100

    Update deploy.sh

--- exercise1/deploy.sh
+++ exercise1/deploy.sh
@@ -1,1 +1,1 @@
-docker stack deploy -c docker-compose.yml -c docker-compose.local-overrides.yml -c 10sph.yaml hub-exercise
+docker stack deploy -c $exercise/docker-compose.yml -c $exercise/docker-compose.local-overrides.yml -c $exercise/10sph.yaml hub-exercise
