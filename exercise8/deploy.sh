a87efeef8d34e965d59484b53dff48c626988c8d
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 15:47:23 +0100

    Add files via upload

---
+++ exercise8/deploy.sh
@@ -0,0 +1,5 @@
+docker run -d --rm -v $1/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0
+while ! $(docker inspect -f {{.State.Running}} $(docker ps -q -f name=mitmproxy)) ; do sleep 0.1; done;
+docker secret create hub-exercise_HUB_PROXY_CERT_FILE $1/.mitmproxy/mitmproxy-ca-cert.pem
+sed -i 's/sHUB_PROXY_HOST=/HUB_PROXY_HOST=$(hostname)/'
+docker stack deploy -c $1/docker-compose_2.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
