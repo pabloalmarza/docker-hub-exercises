0629bb74acc88d8b1bf66f31c2304daeb7a269e0
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 19:52:06 +0100

    Add files via upload

---
+++ exercise9/deploy.sh
@@ -0,0 +1,8 @@
+docker kill $(docker ps  -f ancestor=mitmproxy/mitmproxy -q)
+docker secret rm hub-exercise_HUB_PROXY_CERT_FILE
+docker run -d --rm -v $PWD/$1/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0
+while ! $(docker inspect -f {{.State.Running}} $(docker ps  -f ancestor=mitmproxy/mitmproxy -q)) ; do sleep 0.1; done;
+sleep 5
+docker secret create hub-exercise_HUB_PROXY_CERT_FILE $PWD/$1/.mitmproxy/mitmproxy-ca-cert.pem
+sed -i "s/EMPTY/$HOSTNAME/g" $1/blackduck-config.env
+docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
