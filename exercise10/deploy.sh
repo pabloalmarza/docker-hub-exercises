b6f46aaabd76f2214a866ff1f5d5bb1878d1305d
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 13:05:26 +0100

    Update deploy.sh

--- exercise10/deploy.sh
+++ exercise10/deploy.sh
@@ -1,5 +1,5 @@
 docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
-while [ $(docker cp healthcheck.sh $(docker ps -q -f name=cfssl)) != "healthy" ]; do sleep 0.1; done;
+while ! $(docker inspect -f {{.State.Running}} $(docker ps -q -f name=cfssl)) ; do sleep 0.1; done;
 docker cp $1/healthcheck.sh $(docker ps -q -f name=cfssl):/usr/local/bin/docker-healthcheck.sh
 docker commit $(docker ps -q -f name=cfssl) blackducksoftware/blackduck-cfssl:1.0.7
 /bin/bash delete.sh
