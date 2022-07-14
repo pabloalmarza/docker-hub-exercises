dfe492d822e2b4e3aae18fd365d4c7537cd7dd99
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 16:04:38 +0100

    Update deploy.sh

--- exercise8/deploy.sh
+++ exercise8/deploy.sh
@@ -1,5 +1,5 @@
 docker kill $(docker ps  -f ancestor=mitmproxy/mitmproxy -q)
-docker run -d --rm -v $1/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0
+docker run -d --rm -v $PWD/$1/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0
 while ! $(docker inspect -f {{.State.Running}} $(docker ps  -f ancestor=mitmproxy/mitmproxy -q)) ; do sleep 0.1; done;
 docker secret create hub-exercise_HUB_PROXY_CERT_FILE $1/.mitmproxy/mitmproxy-ca-cert.pem
 sed -i 's/sHUB_PROXY_HOST=EMPTY/HUB_PROXY_HOST=$(hostname)/'
