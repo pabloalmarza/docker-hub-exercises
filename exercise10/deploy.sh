c2f80ebe071b87f13a1e34565412240bae1d5944
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-14 14:09:44 +0100

    Update deploy.sh

--- exercise10/deploy.sh
+++ exercise10/deploy.sh
@@ -3,7 +3,6 @@ docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-override
 while ! $(docker inspect -f {{.State.Running}} $(docker ps -q -f name=cfssl)) ; do sleep 0.1; done;
 docker cp $1/healthcheck.sh $(docker ps -q -f name=cfssl):/usr/local/bin/docker-healthcheck.sh
 docker commit $(docker ps -q -f name=cfssl) blackducksoftware/blackduck-cfssl:1.0.7.1
-docker save $(docker images -a '--format={{.ID}}' -q | head -n 1) -o cfssl.tar
 stack=hub-exercise
 echo "Deleting stack $stack"
 docker stack rm $stack
@@ -15,7 +14,4 @@ done
 
 sleep 3
 docker volume rm $(docker volume ls -q | grep $stack)
-docker image rm $(docker image ls -f reference='blackducksoftware/blackduck-cfssl' -q)
-docker load -i cfssl.tar
-docker image tag $(docker images -a '--format={{.ID}}' -q | head -n 1) blackducksoftware/blackduck-cfssl:1.0.7.1
 docker stack deploy -c $1/docker-compose_2.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
