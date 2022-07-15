0c1dc5069e8e0568abccfd7b1173b180b4d1e6a0
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-15 11:07:59 +0100

    Delete deploy.sh

--- exercise3/deploy.sh
+++
@@ -1,3 +0,0 @@
-docker secret create hub-exercise_WEBSERVER_CUSTOM_CERT_FILE $1/cert.cer
-docker secret create hub-exercise_WEBSERVER_CUSTOM_KEY_FILE $1/key.pem
-docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
