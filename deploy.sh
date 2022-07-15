e9238f6cff7df0d4652773ce24bc2a2eb18a8d7d
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-15 11:06:00 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-exercise="exercise$1";
-if [ -d $exercise ]
-then
-	chmod 777 $exercise/deploy.sh
-	/bin/bash $exercise/deploy.sh $exercise
-else
-  echo "Error: Directory $exercise does not exists."
-fi
