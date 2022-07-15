82458c1cfb3547fe2cbbdbd511e07e5ebdfd2654
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-07-15 11:10:54 +0100

    Delete deploy.sh

--- exercise7/deploy.sh
+++
@@ -1,3 +0,0 @@
-docker stack deploy -c $1/docker-compose.yml -c $1/docker-compose.local-overrides.yml -c $1/10sph.yaml hub-exercise
-while ! $(docker inspect -f {{.State.Running}} $(docker ps  -f name="postgres.1" -q)) ; do sleep 0.1; done;
-docker exec -it $(docker ps  -f name="postgres.1" -q) psql -U blackduck -d bds_hub -c "update st.usermgmt_enduser set active='t' where name='sysadmin';"
